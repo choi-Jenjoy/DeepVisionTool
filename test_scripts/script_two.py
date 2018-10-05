1c9d2e787794c71ee9d1f327ebbb59a6c99a5adf
ashishrana160796 <ashishrana160796@gmail.com>
2018-10-06 03:12:10 +0530

    Testing Scripts Added

---
+++ test_scripts/script_two.py
@@ -0,0 +1,8 @@
+def main():
+    print("Context Activity Classification Module Two Test Successful!!")
+    sample_txt = input("Test, With Some Text !!")
+    print ("Testing Complete, %s." % sample_txt)
+  
+if __name__== "__main__":
+  main()
+
