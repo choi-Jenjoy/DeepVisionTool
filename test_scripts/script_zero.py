64253f352aca1016e6b720307abae4ea41c1bfd0
tripleee <tripleee@users.noreply.github.com>
2018-10-30 11:16:19 +0200

    Trivial English & orthography fixes

--- test_scripts/script_zero.py
+++ test_scripts/script_zero.py
@@ -1,6 +1,6 @@
 def main():
     print("NLP Chatbot Zeus Module Zero Test Successful!!")
-    sample_txt = input("Test, With Some Text !!")
+    sample_txt = input("Test, With Some Text!!")
     print ("Testing Complete, %s." % sample_txt)
   
 if __name__== "__main__":
